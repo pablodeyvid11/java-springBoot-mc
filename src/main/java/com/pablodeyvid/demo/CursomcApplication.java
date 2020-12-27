f88e9589a93298541e4cbfdf36b0d2c56f338084
Pablo Deyvid <pablodeyvid11@gmail.com>
2020-12-26 21:01:10 -0300

    primeiro commit

---
+++ src/main/java/com/pablodeyvid/demo/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.pablodeyvid.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
