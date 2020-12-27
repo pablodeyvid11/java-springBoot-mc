53896ab83da4855314abb3ceb34dcea22c66392a
Pablo Deyvid <pablodeyvid11@gmail.com>
2020-12-26 21:32:51 -0300

    Domain Class: Categoria - Tested

--- src/main/java/com/pablodeyvid/demo/resources/CategoriaResource.java
+++ src/main/java/com/pablodeyvid/demo/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.pablodeyvid.demo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pablodeyvid.demo.domain.Categoria;
 
 @RestController
 @RequestMapping(value="categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "Rest está funcionando!";
+	public List<Categoria> listar() {
+		Categoria c1 = new Categoria(1, "Eletrônicos");
+		Categoria c2 = new Categoria(1, "Ferramentas");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(c1);
+		categorias.add(c2);
+		
+		
+		return categorias;
 	}
 }
