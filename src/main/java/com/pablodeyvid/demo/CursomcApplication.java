61aa85deaf828d4424b2e24a820f14f8f9d43b17
Pablo Deyvid <pablodeyvid11@gmail.com>
2021-04-03 17:25:33 -0300

    CategoriaResourse -> CategoriaController, add getAll method and creating instantiation operation

--- src/main/java/com/pablodeyvid/demo/CursomcApplication.java
+++ src/main/java/com/pablodeyvid/demo/CursomcApplication.java
@@ -1,13 +1,30 @@
 package com.pablodeyvid.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.pablodeyvid.demo.domain.Categoria;
+import com.pablodeyvid.demo.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
